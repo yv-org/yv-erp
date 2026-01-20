91d8d9f6bbcba1062d635e597ab21831549b97b5
MS-Official <smahdiya.official@gmail.com>
2026-01-20 10:28:45 +0530

    chore: add dashboard directory and update .DS_Store

---
+++ dashboard/src/app/modules/auth/confirmation-required/confirmation-required.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { fuseAnimations } from '@fuse/animations';
+
+@Component({
+    selector: 'auth-confirmation-required',
+    templateUrl: './confirmation-required.component.html',
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations,
+    imports: [RouterLink],
+})
+export class AuthConfirmationRequiredComponent {
+    /**
+     * Constructor
+     */
+    constructor() {}
+}
